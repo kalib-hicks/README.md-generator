fa9fb1a26aea1086537507e3cb585648eac6cea4
Kalib Hicks <kalib.hicks@gmail.com>
2021-02-23 20:30:06 -0500

    license

--- Develop/YourREADME.md
+++ Develop/YourREADME.md
@@ -1,6 +1,8 @@
 
   # y
     
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
 ## Table of Contents
 * [Description](#description)
 * [Instructions](#instructions)
@@ -31,7 +33,7 @@ y
 y
 
 ## License
-
+Apache 2.0 License
 
 ## Questions
 Any questions can be directed to me here: 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,10 @@
-
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.titleInput}
     
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
 ## Table of Contents
 * [Description](#description)
 * [Instructions](#instructions)
@@ -35,7 +35,9 @@ ${data.testInput}
 ${data.testCredits}
 
 ## License
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 ${data.licenseInput}
+[Visit the license website](https://opensource.org/licenses/Apache-2.0)
 
 ## Questions
 Any questions can be directed to me here: 
